aa33e0c3eece12b0b47f094a6077fc16d1905c0e
Marian Eichholz <marian.eichholz@freenet.ag>
2013-08-26 14:16:51 +0200

    options ausgelagert.

--- tests/unit/test_app.cc
+++ tests/unit/test_app.cc
@@ -12,44 +12,6 @@ class App_Fixture : public ::testing::Test {
     }
 };
 
-typedef enum { of_default=0, of_decimal=0, of_hexadecimal, of_binary, of_float } OptionFlags;
-
-class OptionParser {
-    public:
-             OptionParser() {};
-    virtual ~OptionParser() {};
-    virtual void on (char o_short, char const *o_long,
-            bool &value_out, bool def_value,
-            char const *description=nullptr, OptionFlags of=of_default);
-    virtual void on (char o_short, char const *o_long,
-            unsigned int &value_out, unsigned int def_value,
-            char const *description=nullptr, OptionFlags of=of_default);
-};
-
-void OptionParser::on (char o_short, char const *o_long,
-       bool &value_out, bool def_value, char const *description, OptionFlags of)
-{
-    cout << "registering " << o_long << " as bool." << endl;
-    // TODO: push a new option record
-}
-
-void OptionParser::on (char o_short, char const *o_long,
-       unsigned int &value_out, unsigned int def_value, char const *description, OptionFlags of)
-{
-    cout << "registering " << o_long << " as unsigned int." << endl;
-    // TODO: push a new option record
-}
-
-TEST_F(App_Fixture, options) {
-    OptionParser op;
-    unsigned int dmask;
-    bool verbose;
-    // TODO: pack value and default into typed pair instance
-    op.on ('v', "verbose", verbose, false, "verbose output");
-    op.on ('d', "debug", dmask, 0u, "debugging mask");
-    FAIL();
-}
-
 TEST_F(App_Fixture, greeter) {
     EXPECT_STREQ("Hello, World!", ::theApp->getGreeter());
 }
